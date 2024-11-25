960a4863d4a76051dafc055ae8e2f4ac1841e84a
Om Thube <om.787thube@gmail.com>
2024-11-25 23:05:36 +0530

    first commit

---
+++ src/main/java/com/mobile/oneplus/OneplusApplication.java
@@ -0,0 +1,13 @@
+package com.mobile.oneplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobile/oneplus/OneplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.oneplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
